9dc547ac82a32a2a377ce026e13db81ccd1dac7d
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-07-27 19:30:58 +0530

    exceptional_server

--- code.cpp
+++ code.cpp
@@ -1,114 +1,56 @@
-//https://www.hackerrank.com/challenges/virtual-functions/problem
+//https://www.hackerrank.com/challenges/exceptional-server
 
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
 using namespace std;
-class Person{
-  public:
-    string name;
-    int age;
-    virtual void getdata()
-    {
-       cin>>name;
-       cin>>age;
-    }
-    virtual void putdata()
-    {
-       cout<<name<<" ";
-       cout<<age<<" ";
-    }
+
+class Server {
+private:
+	static int load;
+public:
+	static int compute(long long A, long long B) {
+		load += 1;
+		if(A < 0) {
+			throw std::invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if(B == 0) throw 0;
+		real = (A/B)*real;
+		int ans = v.at(B);
+		return real + A - B*ans;
+	}
+	static int getLoad() {
+		return load;
+	}
 };
+int Server::load = 0;
 
-class Professor : public Person {
-   private:
-    int publications;
-    static int cur_id;
-    int id;
-    
-   public:
-    Professor()
-    {
-        cur_id++;
-        id = cur_id;
-    }
-    
-    void getdata()
-    {
-        Person::getdata();
-        cin>>publications;
-    }
-    
-    void putdata()
-    {
-        Person::putdata();
-        cout<<publications<<" "<<id<<endl;
-    }
-};
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		long long A, B;
+		cin >> A >> B;
 
-int Professor::cur_id = 0;
-
-class Student : public Person{
-  private:
-  #define SIZE 6
-  vector<int> v;
-  static int cur_id;
-  int id;
-  
-  public:
-  Student()
-  {
-      cur_id++;
-      id = cur_id;
-  }  
-  void getdata()
-  {
-      Person::getdata();
-      for(int i=0; i<SIZE; i++)
-      {
-          int score;
-          cin>>score;
-          v.push_back(score);
-      }
-  }
-  
-  void putdata()
-  {
-      int marks_sum = 0;
-      for(int &a : v)
-          marks_sum += a;
-      Person::putdata();
-      cout<<marks_sum<<" "<<id<<endl;
-  }
-};
-
-int Student::cur_id = 0;
-
-int main(){
-
-    int n, val;
-    cin>>n; //The number of objects that is going to be created.
-    Person *per[n];
-
-    for(int i = 0;i < n;i++){
-
-        cin>>val;
-        if(val == 1){
-            // If val is 1 current object is of type Professor
-            per[i] = new Professor;
-
+		/* Enter your code here. */
+        try {
+            cout << Server::compute(A,B) << endl;
+        } 
+        catch (bad_alloc error) {
+            cout << "Not enough memory" << endl;
         }
-        else per[i] = new Student; // Else the current object is of type Student
+        catch (exception& error) {
+            cout << "Exception: " << error.what() << endl;
+        }
+        catch (...) {
+            cout << "Other Exception" << endl;
+        }
 
-        per[i]->getdata(); // Get the data from the user.
-
-    }
-
-    for(int i=0;i<n;i++)
-        per[i]->putdata(); // Print the required output for each object.
-
-    return 0;
-
+	}
+	cout << Server::getLoad() << endl;
+	return 0;
 }
