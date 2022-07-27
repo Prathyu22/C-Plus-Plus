5339e1cc8560e7ff78f58c3eb3e3985b61b0893d
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-07-27 19:33:37 +0530

    accessing_inherited_functions

--- code.cpp
+++ code.cpp
@@ -1,31 +1,125 @@
-//https://www.hackerrank.com/challenges/vector-sort/problem
+//https://www.hackerrank.com/challenges/accessing-inherited-functions
 
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+
 using namespace std;
 
+class A
+{
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    vector<int> v;
-    int size;
-    cin>>size;
-    
-    for(int i=0; i<size; i++)
-    {
-        int ele;
-        cin>>ele;
-        v.push_back(ele);
-    }
-    
-    sort(v.begin(),v.end());
-    
-    for(int i=0; i<size; i++)
-    {
-        cout<<v[i]<<" ";
-    }
-    return 0;
+    protected:
+        void func(int & a)
+        {
+            a = a * 2;
+            inc();
+        }
+    public:
+        int getA(){
+            return callA;
+        }
+};
+
+class B
+{
+    public:
+        B(){
+            callB = 0;
+        }
+    private:
+        int callB;
+        void inc(){
+            callB++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 3;
+            inc();
+        }
+    public:
+        int getB(){
+            return callB;
+        }
+};
+
+class C
+{
+    public:
+        C(){
+            callC = 0;
+        }
+    private:
+        int callC;
+        void inc(){
+            callC++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 5;
+            inc();
+        }
+    public:
+        int getC(){
+            return callC;
+        }
+};
+
+class D : public A, public B, public C
+{
+
+	int val;
+	public:
+		//Initially val is 1
+		 D()
+		 {
+		 	val = 1;
+		 }
+
+
+		 //Implement this function
+		 void update_val(int new_val)
+		 {
+             while(new_val%2 == 0) {
+                A::func(val);
+                new_val /= 2;
+            }
+            while(new_val%3 == 0) {
+                B::func(val);
+                new_val /= 3;
+            }
+            while(new_val%5 == 0) {
+                C::func(val);
+                new_val /= 5;
+            }			
+		 }
+		 //For Checking Purpose
+		 void check(int); //Do not delete this line.
+};
+
+
+
+void D::check(int new_val)
+{
+    update_val(new_val);
+    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+}
+
+
+int main()
+{
+    D d;
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);
+
 }
