7892341dd856875c20fe1fa7fffdacd21923733a
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-07-27 19:37:18 +0530

    lower_bound

--- code.cpp
+++ code.cpp
@@ -7,58 +7,37 @@
 #include <algorithm>
 using namespace std;
 
-class Matrix{
-    public:
-    vector<vector<int>> a;
-    
-    //The best way to pass in "read-only" paremeters is by const reference (&).
-    Matrix operator + (const Matrix &y)
-    {
-        for(int i=0; i<y.a.size(); i++)
-        {
-            for(int j=0; j<y.a[0].size(); j++)
-            {
-                this->a[i][j] = this->a[i][j] + y.a[i][j];
-            }
-        }
-        return *this;
-    }    
-};
 
-int main () {
-   int cases,k;
-   cin >> cases;
-   for(k=0;k<cases;k++) {
-      Matrix x;
-      Matrix y;
-      Matrix result;
-      int n,m,i,j;
-      cin >> n >> m;
-      for(i=0;i<n;i++) {
-         vector<int> b;
-         int num;
-         for(j=0;j<m;j++) {
-            cin >> num;
-            b.push_back(num);
-         }
-         x.a.push_back(b);
-      }
-      for(i=0;i<n;i++) {
-         vector<int> b;
-         int num;
-         for(j=0;j<m;j++) {
-            cin >> num;
-            b.push_back(num);
-         }
-         y.a.push_back(b);
-      }
-      result = x+y;
-      for(i=0;i<n;i++) {
-         for(j=0;j<m;j++) {
-            cout << result.a[i][j] << " ";
-         }
-         cout << endl;
-      }
-   }  
-   return 0;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0; i<n; i++)
+    {
+        int num;
+        cin>>num;
+        v.push_back(num);
+    }
+    
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int n1;
+        cin>>n1;
+        vector<int>::iterator iter;
+        iter = lower_bound(v.begin(), v.end(), n1);
+        if( *iter == n1)
+        {
+            cout<<"Yes"<<" "<<iter - v.begin() +1<<endl;
+        }
+        else 
+        {
+            cout<<"No"<<" "<<iter - v.begin() +1<<endl;
+        }
+        
+        
+    }
+    return 0;
 }
