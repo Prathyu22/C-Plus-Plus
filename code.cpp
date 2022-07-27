a231ae38abf3cf88934557d92f7ce2cdef89773b
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-07-27 19:29:32 +0530

    virtual_functions

--- code.cpp
+++ code.cpp
@@ -1,56 +1,114 @@
-//https://www.hackerrank.com/challenges/inherited-code/problem?isFullScreen=true
+//https://www.hackerrank.com/challenges/virtual-functions/problem
 
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <exception>
+#include <algorithm>
 using namespace std;
-
-/* Define the exception here */
-class BadLengthException{
-    private:
-    int n;
-    public:
-    BadLengthException(int n1)
+class Person{
+  public:
+    string name;
+    int age;
+    virtual void getdata()
     {
-        n = n1;
+       cin>>name;
+       cin>>age;
     }
-    int what()
+    virtual void putdata()
     {
-        return n;
+       cout<<name<<" ";
+       cout<<age<<" ";
     }
 };
 
-bool checkUsername(string username) {
-	bool isValid = true;
-	int n = username.length();
-	if(n < 5) {
-		throw BadLengthException(n);
-	}
-	for(int i = 0; i < n-1; i++) {
-		if(username[i] == 'w' && username[i+1] == 'w') {
-			isValid = false;
-		}
-	}
-	return isValid;
-}
+class Professor : public Person {
+   private:
+    int publications;
+    static int cur_id;
+    int id;
+    
+   public:
+    Professor()
+    {
+        cur_id++;
+        id = cur_id;
+    }
+    
+    void getdata()
+    {
+        Person::getdata();
+        cin>>publications;
+    }
+    
+    void putdata()
+    {
+        Person::putdata();
+        cout<<publications<<" "<<id<<endl;
+    }
+};
 
-int main() {
-	int T; cin >> T;
-	while(T--) {
-		string username;
-		cin >> username;
-		try {
-			bool isValid = checkUsername(username);
-			if(isValid) {
-				cout << "Valid" << '\n';
-			} else {
-				cout << "Invalid" << '\n';
-			}
-		} catch (BadLengthException e) {
-			cout << "Too short: " << e.what() << '\n';
-		}
-	}
-	return 0;
+int Professor::cur_id = 0;
+
+class Student : public Person{
+  private:
+  #define SIZE 6
+  vector<int> v;
+  static int cur_id;
+  int id;
+  
+  public:
+  Student()
+  {
+      cur_id++;
+      id = cur_id;
+  }  
+  void getdata()
+  {
+      Person::getdata();
+      for(int i=0; i<SIZE; i++)
+      {
+          int score;
+          cin>>score;
+          v.push_back(score);
+      }
+  }
+  
+  void putdata()
+  {
+      int marks_sum = 0;
+      for(int &a : v)
+          marks_sum += a;
+      Person::putdata();
+      cout<<marks_sum<<" "<<id<<endl;
+  }
+};
+
+int Student::cur_id = 0;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
 }
